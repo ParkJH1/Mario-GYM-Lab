787118a9e9ac04cab138b70fa74b2106468d6d44
박정현 <pjh9996@naver.com>
2023-11-04 11:11:17 +0900

    Create 02. input_key.py

---
+++ mario/02. input_key.py
@@ -0,0 +1,15 @@
+import retro
+import numpy as np
+
+env = retro.make(game='SuperMarioBros-Nes', state='Level1-1')
+env.reset()
+
+ram = env.get_ram()
+print(ram[0x03AD])
+
+for i in range(50):
+    # B, NULL, SELECT, START, U, D, L, R, A
+    env.step(np.array([0, 0, 0, 0, 0, 0, 0, 1, 0]))
+
+ram = env.get_ram()
+print(ram[0x03AD])
