45fa745432c0ec0608da0fe06a87855507dfb0de
박정현 <pjh9996@naver.com>
2023-11-04 15:32:44 +0900

    Update 02. input_key.py

--- mario/02. input_key.py
+++ mario/02. input_key.py
@@ -7,7 +7,7 @@ env.reset()
 ram = env.get_ram()
 print(ram[0x03AD])
 
-for i in range(50):
+for i in range(20):
     # B, NULL, SELECT, START, U, D, L, R, A
     env.step(np.array([0, 0, 0, 0, 0, 0, 0, 1, 0]))
 
