17dcd0c66c31d62cb447c72e1102c2c75943b73a
박정현 <pjh9996@naver.com>
2023-11-11 05:13:57 +0900

    Create 08. status.py

---
+++ mario/08. status.py
@@ -0,0 +1,39 @@
+import retro
+import numpy as np
+
+env = retro.make(game='SuperMarioBros-Nes', state='Level1-1')
+env.reset()
+
+for i in range(1000):
+    ram = env.get_ram()
+
+    # 0x001D	Player "float" state
+    # 0x03 - 클리어
+    player_float_state = ram[0x001D]
+    print(player_float_state)
+
+    if player_float_state == 0x03:
+        print('클리어')
+        break
+
+    # 0x000E	Player's state
+    # 0x06, 0x0B - 게임 오버
+    player_state = ram[0x000E]
+    print(player_state)
+
+    if player_state == 0x06 or player_state == 0x0B:
+        print('게임 오버')
+        break
+
+    # 0x00B5	Player vertical screen position
+    # anywhere below viewport is >1
+    player_vertical_screen_position = ram[0x00B5]
+    print(player_vertical_screen_position)
+
+    if player_vertical_screen_position >= 2:
+        print('낙사')
+        break
+
+    print('-------------------')
+
+    env.step(np.array([0, 0, 0, 0, 0, 0, 0, 1, 0]))
