3f52bd76c1d4648298aa49fea0f3c85833dfa679
박정현 <pjh9996@naver.com>
2023-11-16 17:46:33 +0900

    Create 03. load_model.py

---
+++ ai/03. load_model.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+
+def relu(x):
+    return np.maximum(0, x)
+
+
+def sigmoid(x):
+    return 1.0 / (1.0 + np.exp(-x))
+
+
+class Model:
+    def __init__(self):
+        self.w1 = np.random.uniform(low=-1, high=1, size=(13 * 16, 11))
+        self.b1 = np.random.uniform(low=-1, high=1, size=(11,))
+
+        self.w2 = np.random.uniform(low=-1, high=1, size=(11, 6))
+        self.b2 = np.random.uniform(low=-1, high=1, size=(6,))
+
+    def predict(self, data):
+        layer1 = relu(np.matmul(data, self.w1) + self.b1)
+        output = sigmoid(np.matmul(layer1, self.w2) + self.b2)
+
+        print(output)
+
+        result = (output > 0.5).astype(np.int)
+        print(result)
+
+        return result
+
+
+if __name__ == '__main__':
+    model = Model()
+
+    model.w1 = np.load('../model/w1.npy')
+    model.b1 = np.load('../model/b1.npy')
+    model.w2 = np.load('../model/w2.npy')
+    model.b2 = np.load('../model/b2.npy')
+
+    data = [i for i in range(13 * 16)]
+    model.predict(data)
