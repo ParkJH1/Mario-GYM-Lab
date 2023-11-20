b559c0b037a62cff8f2c9f427e86c96b5f10d9e1
박정현 <pjh9996@naver.com>
2023-11-21 08:39:11 +0900

    Update 07.py

--- challenge/07.py
+++ challenge/07.py
@@ -73,7 +73,6 @@ class MarioAI(QWidget):
         self.model.b2 = np.load('../model/level1/b2.npy')
 
     def timer(self):
-        self.env.step(np.array(self.press_buttons))
         self.update_screen()
         self.update()
 
@@ -173,56 +172,53 @@ class MarioTileMap(QWidget):
         painter = QPainter()
         painter.begin(self)
 
-        try:
-            ram = self.main.mario.env.get_ram()
+        ram = self.main.mario.env.get_ram()
 
-            full_screen_tiles = ram[0x0500:0x069F + 1]
-            full_screen_tile_count = full_screen_tiles.shape[0]
+        full_screen_tiles = ram[0x0500:0x069F + 1]
+        full_screen_tile_count = full_screen_tiles.shape[0]
 
-            full_screen_page1_tiles = full_screen_tiles[:full_screen_tile_count // 2].reshape((-1, 16))
-            full_screen_page2_tiles = full_screen_tiles[full_screen_tile_count // 2:].reshape((-1, 16))
+        full_screen_page1_tiles = full_screen_tiles[:full_screen_tile_count // 2].reshape((-1, 16))
+        full_screen_page2_tiles = full_screen_tiles[full_screen_tile_count // 2:].reshape((-1, 16))
 
-            full_screen_tiles = np.concatenate((full_screen_page1_tiles, full_screen_page2_tiles), axis=1).astype(np.int)
+        full_screen_tiles = np.concatenate((full_screen_page1_tiles, full_screen_page2_tiles), axis=1).astype(np.int)
 
-            enemy_drawn = ram[0x000F:0x0014]
-            enemy_horizontal_position_in_level = ram[0x006E:0x0072 + 1]
-            enemy_x_position_on_screen = ram[0x0087:0x008B + 1]
-            enemy_y_position_on_screen = ram[0x00CF:0x00D3 + 1]
+        enemy_drawn = ram[0x000F:0x0014]
+        enemy_horizontal_position_in_level = ram[0x006E:0x0072 + 1]
+        enemy_x_position_on_screen = ram[0x0087:0x008B + 1]
+        enemy_y_position_on_screen = ram[0x00CF:0x00D3 + 1]
 
-            for i in range(5):
-                if enemy_drawn[i] == 1:
-                    ex = (((enemy_horizontal_position_in_level[i] * 256) + enemy_x_position_on_screen[i]) % 512 + 8) // 16
-                    ey = (enemy_y_position_on_screen[i] - 8) // 16 - 1
-                    if 0 <= ex < full_screen_tiles.shape[1] and 0 <= ey < full_screen_tiles.shape[0]:
-                        full_screen_tiles[ey][ex] = -1
+        for i in range(5):
+            if enemy_drawn[i] == 1:
+                ex = (((enemy_horizontal_position_in_level[i] * 256) + enemy_x_position_on_screen[i]) % 512 + 8) // 16
+                ey = (enemy_y_position_on_screen[i] - 8) // 16 - 1
+                if 0 <= ex < full_screen_tiles.shape[1] and 0 <= ey < full_screen_tiles.shape[0]:
+                    full_screen_tiles[ey][ex] = -1
 
-            current_screen_in_level = ram[0x071A]
-            screen_x_position_in_level = ram[0x071C]
-            screen_x_position_offset = (256 * current_screen_in_level + screen_x_position_in_level) % 512
-            sx = screen_x_position_offset // 16
+        current_screen_in_level = ram[0x071A]
+        screen_x_position_in_level = ram[0x071C]
+        screen_x_position_offset = (256 * current_screen_in_level + screen_x_position_in_level) % 512
+        sx = screen_x_position_offset // 16
 
-            screen_tiles = np.concatenate((full_screen_tiles, full_screen_tiles), axis=1)[:, sx:sx + 16]
+        screen_tiles = np.concatenate((full_screen_tiles, full_screen_tiles), axis=1)[:, sx:sx + 16]
 
-            painter.setPen(QPen(Qt.GlobalColor.black, 2, Qt.PenStyle.SolidLine))
-            for i in range(screen_tiles.shape[0]):
-                for j in range(screen_tiles.shape[1]):
-                    if screen_tiles[i][j] > 0:
-                        screen_tiles[i][j] = 1
-                    if screen_tiles[i][j] == -1:
-                        screen_tiles[i][j] = 2
-                        painter.setBrush(QBrush(Qt.GlobalColor.red))
-                    else:
-                        painter.setBrush(QBrush(QColor.fromHslF(125 / 239, 0 if screen_tiles[i][j] == 0 else 1, 120 / 240)))
-                    painter.drawRect(20 * j, 20 * i, 20, 20)
+        painter.setPen(QPen(Qt.GlobalColor.black, 2, Qt.PenStyle.SolidLine))
+        for i in range(screen_tiles.shape[0]):
+            for j in range(screen_tiles.shape[1]):
+                if screen_tiles[i][j] > 0:
+                    screen_tiles[i][j] = 1
+                if screen_tiles[i][j] == -1:
+                    screen_tiles[i][j] = 2
+                    painter.setBrush(QBrush(Qt.GlobalColor.red))
+                else:
+                    painter.setBrush(QBrush(QColor.fromHslF(125 / 239, 0 if screen_tiles[i][j] == 0 else 1, 120 / 240)))
+                painter.drawRect(20 * j, 20 * i, 20, 20)
 
-            player_x_position_current_screen_offset = ram[0x03AD]
-            player_y_position_current_screen_offset = ram[0x03B8]
-            px = (player_x_position_current_screen_offset + 8) // 16
-            py = (player_y_position_current_screen_offset + 8) // 16 - 1
-            painter.setBrush(QBrush(Qt.GlobalColor.blue))
-            painter.drawRect(20 * px, 20 * py, 20, 20)
-        except:
-            pass
+        player_x_position_current_screen_offset = ram[0x03AD]
+        player_y_position_current_screen_offset = ram[0x03B8]
+        px = (player_x_position_current_screen_offset + 8) // 16
+        py = (player_y_position_current_screen_offset + 8) // 16 - 1
+        painter.setBrush(QBrush(Qt.GlobalColor.blue))
+        painter.drawRect(20 * px, 20 * py, 20, 20)
 
         painter.end()
 
